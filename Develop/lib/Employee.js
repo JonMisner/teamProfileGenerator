17d4d396127038f8e8c0d036e3b2569845a38cb9
JonM <jonmisner86@gmail.com>
2020-09-09 19:13:32 -0500

    gitignore and basic employee class constructor

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Employee class
+const Employee = {
+   constructor (name,id,email) {
+      this.name = name,
+      this.id = id,
+      this.email = email
+   }
+};
+
+Employee.prototype.getId = function () {
+   return this.id;
+};
+
+Employee.prototype.getName = function() {
+   return this.name;
+};
+
+Employee.prototype.getEmail = function() {
+   return this.email
+};
+
+Employee.prototype.getRole = function() {
+   return "Employee"
+};
